1d5d83e8eef65c14231eaa93182c362e8c6a0086
Ivánczi Ferenc <ivanczif21@ADY-NAGYATAD.SULINET.HU>
2022-09-23 14:02:48 +0200

    alevibuzi

---
+++ html/lista2.py
@@ -0,0 +1,20 @@
+szavak=["16","bakfitty","csutora",]
+szamok=[1,0,3000,2]
+
+print(szavak)
+
+szavak.sort()
+print(szavak)
+
+szavak.append("hát")
+print(szavak)
+
+szavak.insert(1,"az hogy")
+print(szavak)
+
+szamok.append(20)
+szamok.append(20)
+
+darab=szamok.count(20)
+
+print(darab)
