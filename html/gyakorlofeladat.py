ffd3d073860cafb60706e312cd50e1dc3ff6893b
Ivánczi Ferenc <ivanczif21@ADY-NAGYATAD.SULINET.HU>
2022-10-10 13:15:56 +0200

    asd2

--- html/gyakorlofeladat.py
+++ html/gyakorlofeladat.py
@@ -1,4 +1,10 @@
 beSzam=0
 
-while beSzam<10 or beSzam>20 :
-    beSzam= input("Adjon meg egy szamot 10 és 20 kozott: ")
+while beSzam<2 or beSzam>5 :
+    beSzam= int(input("Adjon meg egy szamot 2 és 5 kozott: "))
+
+autok=[]
+for i in range(0, beSzam):
+    autok.append(input ("Kérek egy auto markat: "))
+
+print (autok)
