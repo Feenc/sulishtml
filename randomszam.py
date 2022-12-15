e170118615c7cc953c1516d26fe00dd2f75592b7
Ivánczi Ferenc <ivanczif21@ADY-NAGYATAD.SULINET.HU>
2022-12-15 13:20:02 +0100

    marcibuzi

--- randomszam.py
+++ randomszam.py
@@ -43,3 +43,17 @@ print((random.randint(83,832)*2+1) * 6 )
 szavak=["alma","körte","barack","banón","dinnye","szőlő"]
 print(szavak[random.randint(0,len(szavak)-1)])
 print(random.choice(szavak))
+print("-"*20)
+for e in szavak:
+     #print(e)
+    kisLista=[]
+    kisLista.append(e)
+    kisLista.append(random.randint(12,312))
+    #print(kisLista)
+    nagyLista.append(kisLista)
+
+print(nagyLista)
+
+for e in nagyLista:
+    print(e[0].ljust(10), str(e[1]).rjust(4),"kg","*"*(e[l]//6))
+              
