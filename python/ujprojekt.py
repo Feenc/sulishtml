0a65b21076227ed3bc03b1e3c4e858ee3de6a20d
Ivánczi Ferenc <ivanczif21@ADY-NAGYATAD.SULINET.HU>
2023-01-23 08:44:53 +0100

    asda

--- python/ujprojekt.py
+++ python/ujprojekt.py
@@ -38,20 +38,21 @@ def beolvas():
     f.close()
     
 def kerdez():
+    random.seed(2)
     valasztott=random.choice(kerdesek)
     print("valasztott", valasztott)
     rossz=[]
     for i in range(3):
-        temp=random.choice(kerdesek):
-        print(temp)
-        while temp not in rossz and temo!=valasztott:
+        temp=random.choice(kerdesek)
+        print("temp",temp)
+        while temp not in rossz and temp!=valasztott:
             rossz.append(temp)    
-        print(rossz)
+        print("rossz",rossz)
 
 
     print("-"*40)
     print("Mit jelent: "+ valasztott[0]+"?")
-    rossz.append(valasztott)
+    #rossz.append(valasztott)
     print(rossz)
 beolvas()
 kerdez()
