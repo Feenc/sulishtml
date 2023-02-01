c00b124b10a707ea575c04d876a088c020f2ea56
Ivánczi Ferenc <ivanczif21@ADY-NAGYATAD.SULINET.HU>
2023-02-01 14:03:39 +0100

    ASD

--- erettsegi/szamok.py
+++ erettsegi/szamok.py
@@ -12,4 +12,21 @@ for sor in f:
 f.close()
 
 print("2. feladat")
-print("Az adatfile-ban" + len(kerdesek)+ "kérdés van.")
+print("Az adatfile-ban " + str(len(kerdesek)) + " kérdés van.")
+
+matek=[]
+for e in kerdesek:
+   if e[3]=="matematika":
+       matek.append(e[2])
+
+print(matek)
+print("Az adatfileban " + str(len(matek))+
+      " matematika feladat van, 1pontot er " + str(matek.count(1))+
+      " feladat, 2pontot er " + str(matek.count(2)) +
+      " feladat, 3pontot er " + str(matek.count(3)) + " feladat.")
+
+valaszok=[]
+for e in kerdesek:
+    valaszok.append(e[1])
+valaszok=[e[1] for e in kerdesek]
+print("A válaszok számértéke []t-től []-ig tart.".format(min(valaszok),max(valaszok)))
