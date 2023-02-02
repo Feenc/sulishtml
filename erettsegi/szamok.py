8a70d9301df4cccf8397d6b188af11ae26e848da
Ivánczi Ferenc <ivanczif21@ADY-NAGYATAD.SULINET.HU>
2023-02-02 12:33:41 +0100

    asd

--- erettsegi/szamok.py
+++ erettsegi/szamok.py
@@ -1,3 +1,4 @@
+import random
 f=open("felszam.txt","r")
 
 
@@ -30,3 +31,23 @@ for e in kerdesek:
     valaszok.append(e[1])
 valaszok=[e[1] for e in kerdesek]
 print("A válaszok számértéke []t-től []-ig tart.".format(min(valaszok),max(valaszok)))
+
+temakorok=[]
+for e in kerdesek:
+    if e[3] not in temakorok:
+        temakorok.append(e[3])
+print("5. feladat")
+print("A témakörök: " + ",".join(temakorok))
+
+print("6.feladat: ")
+beker=input("Milyen témakörből szeretne kérdést kapni? ")
+ujlista=[e for e in kerdesek if e[3]==beker]
+random.choice
+sorsolt=random.choice(ujlista)
+
+valasz=input(sorsolt[0])
+if valasz==sorsolt[1]:
+    print(sorsolt[3]" pont")
+else:
+    print("0pont")
+    print("A helyes valasz. "+sorsolt[1])
