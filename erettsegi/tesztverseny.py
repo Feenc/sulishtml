6150b604553ff07799de211fb90079fb2786313c
Ivánczi Ferenc <ivanczif21@ADY-NAGYATAD.SULINET.HU>
2023-02-20 08:16:36 +0100

    Aasd

--- erettsegi/tesztverseny.py
+++ erettsegi/tesztverseny.py
@@ -1,3 +1,12 @@
+def pontSzamit (valasz,helyes):
+    pont=0
+    for sorszam,betu in enumerate(valasz):
+        if betu==helyes[sorszam]:
+            if sorszam<=5:
+                pont+=3
+            elif sorszam<10
+                pont +=4
+
 #1.feladathaha
 f=open("valaszok.txt","r")
 adatok=f.read().split("\n")
@@ -38,3 +47,17 @@ for sorszam,betu in enumerate(versenyzoValasza):
         print(" ",end="")
 
 print("\t(a versenyző helyes válaszai)")
+#print("5.feladat: ")
+feladat=int(input("5.feladat sorszáma:"))
+
+db=0
+for e in valaszok:
+    if e[1][feladat]==helyes[feladat]:
+        db+=1
+    
+print("A feladatra [0] fő, a versenyzők [1:.2%]-a adott helyes választ.".format(db,db/len(valaszok)))
+f=open("pontos.txt","w")
+for e in valaszok:
+    pont=pontSzamit(e[1],helyes)
+
+f.close
