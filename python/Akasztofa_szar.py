a697ad206b0afd56a038229f271d8cfe6ce1cde5
Ivánczi Ferenc <ivanczif21@ADY-NAGYATAD.SULINET.HU>
2023-03-13 11:54:00 +0100

    asd

---
+++ python/Akasztofa_szar.py
@@ -0,0 +1,42 @@
+import random
+
+szavak = ["nokedli", "körte", "tipp", "macska", "kutya","python"]
+szo = random.choice(szavak)
+
+print("Akasztófa játék <3")
+#print("Készítők: Verbulecz Bence,Ivánczi Ferenc,Haraszti Marcell,Keszericze Ákos.")
+rosszTalalat = 0
+maxRosszTalalat = 6
+tippeltBetu = []
+#also="_"
+
+while rosszTalalat < maxRosszTalalat:
+    display_szo = ""
+    for betu in szo:
+        if betu in tippeltBetu:
+            display_szo += betu
+        else:
+            display_szo += "_ "
+    print(display_szo)
+
+    tipp = input("Tippelj egy betűre: ")
+    if len(tipp)==1: 
+        
+        if tipp in tippeltBetu:
+            print("Ezt a betűt már tippelted!")
+        else:
+            tippeltBetu.append(tipp)
+
+            if betu in szo:
+                print("Gratulálok, ez a betű benne van a szóban!")
+                if "_" not in display_szo:
+                    print("Gratulálok, nyertél!")
+                    break
+            else:
+                print("Ez a betű nincs benne a szóban.")
+                rosszTalalat += 1
+
+    print("Hibás tippjeid száma: " + str(rosszTalalat))
+
+    if rosszTalalat == maxRosszTalalat:
+        print("Sajnos vesztettél. A szó: " + szo)
--- python/gyakorlas0307.py
+++ python/gyakorlas0307.py
@@ -10,6 +10,65 @@ gyumolcs[index]+="k"
 print(gyumolcs[3])
 
 print("Rövid gyümölcsök: ")
+
+
+#for i in range(len(gyumolcs)):
+#    if len(gyumolcs[i])<5:
+#        print(gyumolcs[i])
+
+rovid=[]
 for i in range(len(gyumolcs)):
     if len(gyumolcs[i])<5:
-        print(gyumolcs[i])
+        rovid.append(gyumolcs[i])
+print(rovid)
+
+rovid=[]
+for e in gyumolcs:
+    if len(e)<5:
+        rovid.append(e)
+
+print(rovid)
+
+rovid=[e for e in gyumolcs if len(e)<5]
+print(rovid)
+
+rovid=[]
+
+i=0
+
+while i<len(gyumolcs):
+    if len(gyumolcs[i])<5:
+        rovid.append(gyumolcs[i])
+    i+=1
+print(rovid)
+
+rovid=[]
+i=0
+while True:
+    print(i)
+    if len(gyumolcs[i])<5:
+        rovid.append(gyumolcs[i])
+
+    if len(gyumolcs)-1==i:
+        break
+    i+=1
+print(gyumolcs[:2])
+
+print(gyumolcs[-2:])
+
+print(gyumolcs[len(gyumolcs)-2:])
+
+print(gyumolcs[1:-1])
+
+paratlan=gyumolcs[1::2]
+print(paratlan)
+paros=gyumolcs[0::2]
+print(paros)
+
+masolat=gyumolcs
+masolat.reverse()
+print(",".join(masolat))
+
+print(",".join(masolat[:]))
+
+
