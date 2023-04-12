1c392488c0bc77c5e7d2900cc56ecd1fb1c5919d
Ivánczi Ferenc <ivanczif21@ADY-NAGYATAD.SULINET.HU>
2023-04-12 14:49:21 +0200

    ad

---
+++ python/biologia.py
@@ -0,0 +1,42 @@
+"""
+Sivatagi róka (Vulpes zerda)
+Rendszertani besorolás
+Ország:	Állatok (Animalia)
+Törzs:	Gerinchúrosok (Chordata)
+Altörzs:	Gerincesek (Vertebrata)
+Főosztály:	Négylábúak (Tetrapoda)
+Osztály:	Emlősök (Mammalia)
+Alosztály:	Elevenszülő emlősök (Theria)
+Csoport:	Eutheria
+Alosztályág:	Méhlepényesek (Placentalia)
+Öregrend:	Laurasiatheria
+Csoport:	Ferae
+Rend:	Ragadozók (Carnivora)
+Alrend:	Kutyaalkatúak (Caniformia)
+Család:	Kutyafélék (Canidae)
+Alcsalád:	Valódi kutyaformák (Caninae)
+Nemzetség:	Rókák (Vulpini)
+Nem:	Róka (Vulpes)
+Frisch, 1775
+"""
+
+class orszag():
+    def __init__(self,orszag)
+        self.orszag=orszag
+
+class torzs(orszag):
+    def __init__(self,orszag,torzs)
+        super.__init__(orszag)
+        self.torzs=torzs
+class altorzs(torzs):
+    def __init__(self,orszag,torzs,altorzs)
+    super.__init__(orszag,torzs)
+    self.altorzs=altorzs
+class osztaly(altorzs):
+    super.__init__(orszag,torzs,altorzs)
+    self.osztaly=osztaly
+class alosztaly(osztaly):
+    super.__init__(orszag,torzs,altorzs,orzsag)
+    self.alosztaly=alosztaly
+class alosztalyag(alosztaly):
+    super.__init
